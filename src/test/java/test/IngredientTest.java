28f02df6b49db8dfe92a20516c2de135fd7a2576
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2023-01-05 22:37:19 +0300

    add IngredientTest, IngredientTypeTest

--- src/test/java/test/IngredientTest.java
+++ src/test/java/test/IngredientTest.java
@@ -1,4 +1,36 @@
 package test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
+import praktikum.Bun;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
 public class IngredientTest {
+
+    private Ingredient ingredient;
+    private String ingredientName = "ingredientName";
+    private float ingredientPrice = 100.0f;
+
+    @Mock
+    IngredientType type;
+
+
+    @Test
+    public void getPriceForIngredient() {
+        ingredient = new Ingredient(type, ingredientName, ingredientPrice);
+        float expected = ingredientPrice;
+        float actual = ingredient.getPrice();
+        Assert.assertEquals(expected, actual, 0);
+    }
+
+    @Test
+    public void getNameForIngredient() {
+        ingredient = new Ingredient(type, ingredientName, ingredientPrice);
+        String expected = ingredientName;
+        String actual = ingredient.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
 }
