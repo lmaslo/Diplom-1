eb9fcd936259f9018cb22bb10db0e92557c5c8bd
Lena Masloboishchikova <perepechkinaEV@yandex.ru>
2023-01-06 15:48:57 +0300

    add burger test

--- src/test/java/test/BurgerTest.java
+++ src/test/java/test/BurgerTest.java
@@ -1,51 +1,100 @@
 package test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+import praktikum.Burger;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
-    /*
-    public class Burger {
 
-    public Bun bun;
-    public List<Ingredient> ingredients = new ArrayList<>();
+    private Burger burger;
 
-    public void setBuns(Bun bun) {
-        this.bun = bun;
+    @Mock
+    Bun bun;
+    @Mock
+    Ingredient ingredient;
+    @Mock
+    Ingredient ingredientSecond;
+
+    @Test
+    public void setBunForBurger() {
+        burger = new Burger();
+        burger.setBuns(bun);
+        Bun expected = bun;
+        Bun actual = burger.bun;
+        Assert.assertEquals(expected, actual);
     }
 
-    public void addIngredient(Ingredient ingredient) {
-        ingredients.add(ingredient);
+    @Test
+    public void addIngredientAddIngredient() {
+        burger = new Burger();
+        burger.addIngredient(ingredient);
+        Ingredient expected = ingredient;
+        Ingredient actual = burger.ingredients.get(0);
+        Assert.assertEquals(expected, actual);
     }
 
-    public void removeIngredient(int index) {
-        ingredients.remove(index);
+    @Test
+    public void removeIngredientSetIngredientIsEmpty() {
+        burger = new Burger();
+        burger.addIngredient(ingredient);
+        burger.removeIngredient(0);
+        assertTrue(burger.ingredients.isEmpty());
     }
 
-    public void moveIngredient(int index, int newIndex) {
-        ingredients.add(newIndex, ingredients.remove(index));
+    @Test
+    public void moveIngredientChangeIngredient() {
+        burger = new Burger();
+        burger.addIngredient(ingredient);
+        burger.addIngredient(ingredientSecond);
+        burger.moveIngredient(1, 0);
+        Ingredient expected = ingredientSecond;
+        Ingredient actual = burger.ingredients.get(0);
+        Assert.assertEquals(expected, actual);
     }
 
-    public float getPrice() {
-        float price = bun.getPrice() * 2;
 
-        for (Ingredient ingredient : ingredients) {
-            price += ingredient.getPrice();
-        }
-
-        return price;
+    @Test
+    public void getPriceSumBunAndIngredients() {
+        burger = new Burger();
+        burger.setBuns(bun);
+        Mockito.when(bun.getPrice()).thenReturn(50f);
+        burger.addIngredient(ingredient);
+        Mockito.when(ingredient.getPrice()).thenReturn(100f);
+        float expected = 150f;
+        float actual = burger.getPrice();
+        Assert.assertEquals(expected, actual, 0);
     }
 
-    public String getReceipt() {
-        StringBuilder receipt = new StringBuilder(String.format("(==== %s ====)%n", bun.getName()));
+    @Test
+    public void getReceipt() {
+        burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
 
-        for (Ingredient ingredient : ingredients) {
-            receipt.append(String.format("= %s %s =%n", ingredient.getType().toString().toLowerCase(),
-                    ingredient.getName()));
-        }
+        Mockito.when(bun.getName()).thenReturn("bunName");
+        Mockito.when(ingredient.getType()).thenReturn(IngredientType.FILLING);
+        Mockito.when(ingredient.getName()).thenReturn("ingredientName");
+        Mockito.when(burger.getPrice()).thenReturn(200f);
 
-        receipt.append(String.format("(==== %s ====)%n", bun.getName()));
-        receipt.append(String.format("%nPrice: %f%n", getPrice()));
+        String expected =
+                String.format("(==== %s ====)%n", "bunName") +
+                        String.format("= %s %s =%n", ingredient.getType().toString().toLowerCase(), "ingredientName") +
+                        String.format("(==== %s ====)%n", "bunName") +
+                        String.format("%nPrice: %f%n", 200f);
+        String actual = burger.getReceipt();
 
-        return receipt.toString();
+
+        Assert.assertEquals("Receipt incorrect", expected, actual);
     }
 
-}*/
 }
